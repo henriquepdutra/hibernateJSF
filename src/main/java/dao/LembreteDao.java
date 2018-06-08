8ff4bf4bcf0a4dfa273a32bb0771a48d6627474a
Henrique <henrique@datachamp.com.br>
2018-06-08 17:43:19 -0300

    alterações

--- src/main/java/dao/LembreteDao.java
+++ src/main/java/dao/LembreteDao.java
@@ -1,14 +1,97 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
+
+import bean.JpaResourceBean;
+import java.util.List;
+import javax.persistence.EntityManager;
+import model.Lembrete;
 
 /**
  *
  * @author henrique
  */
 public class LembreteDao {
-    
+
+    public List<Lembrete> listarTodos() throws Exception {
+        EntityManager em = JpaResourceBean.getEntityManagerFactory()
+                .createEntityManager();
+        List<Lembrete> lembretes = null;
+
+        try {
+            lembretes = em.createQuery("from Lembrete").getResultList();
+        } catch (Exception e) {
+            throw new Exception(e);
+        } finally {
+            em.close();
+        }
+
+        return lembretes;
+    }
+
+    public void inserir(Lembrete lembrete) throws Exception {
+        EntityManager em = JpaResourceBean.getEntityManagerFactory()
+                .createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(lembrete);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+            throw new Exception(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void atualizar(Lembrete lembrete) throws Exception {
+        EntityManager em = JpaResourceBean.getEntityManagerFactory()
+                .createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.merge(lembrete);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+            throw new Exception(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void excluir(long id) throws Exception {
+        EntityManager em = JpaResourceBean.getEntityManagerFactory()
+                .createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            Lembrete lembrete = em.find(Lembrete.class, id);
+            em.remove(lembrete);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+            throw new Exception(e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Lembrete selecionar(long id) throws Exception {
+        Lembrete lembrete;
+
+        EntityManager em = JpaResourceBean.getEntityManagerFactory()
+                .createEntityManager();
+
+        try {
+            lembrete = em.find(Lembrete.class, id);
+        } finally {
+            em.close();
+        }
+
+        return lembrete;
+    }
+
 }
